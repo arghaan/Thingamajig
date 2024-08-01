40e21c3df548026e15a309cec3b2d04b25dd2850
Syrius <arghaan@gmail.com>
2024-08-01 10:08:03 +0300

    tmp

--- src/main/java/com/syrius/thingamajig/Config.java
+++ src/main/java/com/syrius/thingamajig/Config.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.syrius.thingamajig;
 
 import java.util.List;
 import java.util.Set;
--- src/main/java/com/syrius/thingamajig/ExampleMod.java
+++ src/main/java/com/syrius/thingamajig/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.syrius.thingamajig;
 
 import org.slf4j.Logger;
 
